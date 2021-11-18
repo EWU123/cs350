d532a6c85155e020e74aaae542c7951318f39e1a
Justin Plett <jmplett@me.com>
2021-11-18 11:53:16 -0800

    Abstraction of A_Loader

--- src/atcsim/loader/A_Loader.java
+++ src/atcsim/loader/A_Loader.java
@@ -3,7 +3,7 @@ package atcsim.loader;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.world.navigation.A_ComponentNavaid;
 
-public class A_Loader {
+public abstract class A_Loader {
 	public A_Loader(A_ComponentNavaid<?> navaids, OverlayNavigation overlay) {
 		
 	}
