570b60028cf2dcd3cfb6dfaf39ba58aeebcef506
Zack Ward <79941828+ZackWard-student@users.noreply.github.com>
2021-11-19 12:54:50 -0800

    LoaderFix idea
    Added in the LoaderFix string parser and wrote an example of how CoordinateWorld3D will use it

--- src/atcsim/loader/A_Loader.java
+++ src/atcsim/loader/A_Loader.java
@@ -1,12 +1,15 @@
 package atcsim.loader;
 
 import java.util.Map;
+import java.util.Scanner;
 
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.world.navigation.A_ComponentNavaid;
 
 public abstract class A_Loader {
+	
 	public A_Loader(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
 		
 	}
+	
 }
--- src/atcsim/loader/navaid/LoaderFix.java
+++ src/atcsim/loader/navaid/LoaderFix.java
@@ -2,6 +2,10 @@ package atcsim.loader.navaid;
 
 import java.util.Map;
 
+import atcsim.datatype.Altitude;
+import atcsim.datatype.CoordinateWorld3D;
+import atcsim.datatype.Latitude;
+import atcsim.datatype.Longitude;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
@@ -17,6 +21,14 @@ public class LoaderFix extends A_Loader{
 	}
 
 	public void load(java.util.Scanner scanner) throws java.io.IOException {
+		String line = scanner.nextLine();
+		String[] fields = line.split(", ");
+		String[] lat = fields[1].split(",");
+		String[] lon = fields[2].split(",");
+		
+		new CoordinateWorld3D(	new Latitude(Integer.valueOf(lat[0]), Integer.valueOf(lat[1]), Double.valueOf(lat[2])), 
+								new Longitude(Integer.valueOf(lon[0]), Integer.valueOf(lon[1]), Double.valueOf(lon[2])),
+								new Altitude(Double.valueOf(fields[3])));
 		
 	}
 }
