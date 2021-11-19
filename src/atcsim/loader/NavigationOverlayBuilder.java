3da49a3a30e95b59d67b4029742ed78c7edabd02
Justin Plett <jmplett@me.com>
2021-11-19 11:14:52 -0800

    Stopping point

--- src/atcsim/loader/NavigationOverlayBuilder.java
+++ src/atcsim/loader/NavigationOverlayBuilder.java
@@ -1,13 +1,25 @@
 package atcsim.loader;
 
+import java.util.Scanner;
+
 import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.loader.navaid.LoaderFix;
+import atcsim.loader.navaid.LoaderNDB;
+import atcsim.loader.navaid.LoaderVOR;
 
 public class NavigationOverlayBuilder {
 	public NavigationOverlayBuilder() {
 		
 	}
 	public OverlayNavigation loadDefinition(java.lang.String filespec) throws java.io.IOException {
-		return null;
+		Scanner input = new Scanner(filespec);
+		
+		OverlayNavigation overlay = new OverlayNavigation("overlay");
+		
+		overlay.addNavaid(new LoaderFix(null, overlay));
+		return overlay;
+		
+		
 	}
 	
 	//testing
