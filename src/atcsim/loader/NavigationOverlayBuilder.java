243bcad1c397dfcbf73296fc17a1ff856c232417
Justin Plett <jmplett@me.com>
2021-11-19 11:43:53 -0800

    Start of Load Def

--- src/atcsim/loader/NavigationOverlayBuilder.java
+++ src/atcsim/loader/NavigationOverlayBuilder.java
@@ -1,22 +1,30 @@
 package atcsim.loader;
 
+import java.util.Map;
 import java.util.Scanner;
 
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.navaid.LoaderFix;
 import atcsim.loader.navaid.LoaderNDB;
 import atcsim.loader.navaid.LoaderVOR;
+import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidFix;
+import atcsim.world.navigation.ComponentNavaidNDB;
 
 public class NavigationOverlayBuilder {
 	public NavigationOverlayBuilder() {
-		
+	
 	}
 	public OverlayNavigation loadDefinition(java.lang.String filespec) throws java.io.IOException {
 		Scanner input = new Scanner(filespec);
-		
+		Map<String, A_ComponentNavaid<?>> navaids = null;
 		OverlayNavigation overlay = new OverlayNavigation("overlay");
 		
-		overlay.addNavaid(new LoaderFix(null, overlay)); 
+		input.next();
+
+			LoaderFix fix = new LoaderFix(navaids,overlay);
+			fix.load(input);
+
 		return overlay;
 		
 		
--- src/atcsim/loader/navaid/LoaderFix.java
+++ src/atcsim/loader/navaid/LoaderFix.java
@@ -7,8 +7,13 @@ import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
 
 public class LoaderFix extends A_Loader{
+	Map<String, A_ComponentNavaid<?>> navaids;
+	OverlayNavigation overlay;
+	
 	public LoaderFix(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
 		super(navaids, overlay);
+		this.navaids = navaids;
+		this.overlay = overlay;
 	}
 
 	public void load(java.util.Scanner scanner) throws java.io.IOException {
