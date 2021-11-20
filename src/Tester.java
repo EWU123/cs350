2622b554c573e451039f9367f38bc8d69a17b5de
Justin Plett <jmplett@me.com>
2021-11-20 15:49:20 -0800

    NavigationOverlayBuilder successfully reads file to end
    Also updated tester to pass file into NavigationOverlayBuilder

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,12 @@
+import java.io.IOException;
+
+import atcsim.loader.NavigationOverlayBuilder;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		NavigationOverlayBuilder builder = new NavigationOverlayBuilder();
+		builder.loadDefinition("./definition1.txt");
 	}
 
 }
--- src/atcsim/loader/NavigationOverlayBuilder.java
+++ src/atcsim/loader/NavigationOverlayBuilder.java
@@ -1,10 +1,13 @@
 package atcsim.loader;
 
+import java.io.File;
 import java.util.Map;
 import java.util.Scanner;
 
 import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.loader.navaid.LoaderAirway;
 import atcsim.loader.navaid.LoaderFix;
+import atcsim.loader.navaid.LoaderILS;
 import atcsim.loader.navaid.LoaderNDB;
 import atcsim.loader.navaid.LoaderVOR;
 import atcsim.world.navigation.A_ComponentNavaid;
@@ -16,18 +19,39 @@ public class NavigationOverlayBuilder {
 	
 	}
 	public OverlayNavigation loadDefinition(java.lang.String filespec) throws java.io.IOException {
-		Scanner input = new Scanner(filespec);
+		File fin = new File(filespec);
+		Scanner input = new Scanner(fin);
 		Map<String, A_ComponentNavaid<?>> navaids = null;
 		OverlayNavigation overlay = new OverlayNavigation("overlay");
 		
-		input.next();
+		System.out.println(input.nextLine());
 
-			LoaderFix fix = new LoaderFix(navaids,overlay);
-			fix.load(input);
+		LoaderFix fix = new LoaderFix(navaids,overlay);
+		fix.load(input);
+		
+		nextCatagory(input);
+		LoaderNDB ndb = new LoaderNDB(navaids, overlay);
+		ndb.load(input);
+		
+		nextCatagory(input);
+		LoaderVOR vor= new LoaderVOR(navaids, overlay);
+		vor.load(input);
+		
+		nextCatagory(input);
+		LoaderILS ils = new LoaderILS(navaids, overlay);
+		ils.load(input);
+		
+		nextCatagory(input);
+		LoaderAirway airway = new LoaderAirway(navaids, overlay);
+		airway.load(input);
+
 
 		return overlay;
-		
-		
+	}
+	
+	private void nextCatagory(Scanner input) {
+		System.out.println(input.nextLine());
+		System.out.println(input.nextLine());
 	}
 	
 	//testing
