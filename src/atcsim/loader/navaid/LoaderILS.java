e09fd09c216a914bf2d5fcc0448fe6714eade487
Justin Plett <jmplett@me.com>
2021-11-20 22:10:20 -0800

    LoaderILS Complete

--- src/atcsim/loader/navaid/LoaderILS.java
+++ src/atcsim/loader/navaid/LoaderILS.java
@@ -2,10 +2,14 @@ package atcsim.loader.navaid;
 
 import java.util.Map;
 
+import atcsim.datatype.AngleNavigational;
 import atcsim.datatype.CoordinateWorld3D;
+import atcsim.datatype.VHFFrequency;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidILS;
+import atcsim.world.navigation.NavaidILSBeaconDescriptor;
 
 public class LoaderILS extends A_Loader{
 	public LoaderILS(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
@@ -14,10 +18,27 @@ public class LoaderILS extends A_Loader{
 	
 	public void load(java.util.Scanner scanner) throws java.io.IOException {
 		String line = scanner.nextLine();
-		String[] fields = line.split(", ");
-		String[] lat = fields[2].split(",");
-		String[] lon = fields[3].split(",");
-		
-		CoordinateWorld3D coor3D = pars3D(lat, lon, fields[4]);
+		System.out.println(line);
+		while(line.isBlank() == false)  { 
+			String[] fields = line.split(", ");
+			String id = fields[0];
+			String azimuth = fields[5];
+			String[] lat = fields[2].split(",");
+			String[] lon = fields[3].split(",");
+			String[] frequency = fields[1].split(",");
+			
+			CoordinateWorld3D coor3D = pars3D(lat, lon, fields[4]);
+			VHFFrequency vhf = parsVHF(frequency);
+			AngleNavigational angle = new AngleNavigational(Double.parseDouble(azimuth));
+			NavaidILSBeaconDescriptor outer = parsBeacon(fields[6]);
+			NavaidILSBeaconDescriptor middle = parsBeacon(fields[7]);
+			NavaidILSBeaconDescriptor inner = parsBeacon(fields[8]);
+			
+			ComponentNavaidILS ils = new ComponentNavaidILS(id, coor3D, angle, vhf, outer, middle, inner);
+			
+			overlay.addNavaid(ils);
+			
+			line = scanner.nextLine();
+		}
 	}
 }
