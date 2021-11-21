853373b2cbeb02847047075499e5e62e8d0f5f0f
Justin Plett <jmplett@me.com>
2021-11-20 22:10:05 -0800

    LoaderFix complete

--- src/atcsim/loader/navaid/LoaderFix.java
+++ src/atcsim/loader/navaid/LoaderFix.java
@@ -10,25 +10,33 @@ import atcsim.datatype.Longitude;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidFix;
 
 public class LoaderFix extends A_Loader{
-	Map<String, A_ComponentNavaid<?>> navaids;
-	OverlayNavigation overlay;
 	
 	public LoaderFix(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
 		super(navaids, overlay);
-		this.navaids = navaids;
-		this.overlay = overlay;
+
 	}
 
 	public void load(java.util.Scanner scanner) throws java.io.IOException {
 		String line = scanner.nextLine();
-
-		String[] fields = line.split(", ");
-		String[] lat = fields[1].split(",");
-		String[] lon = fields[2].split(",");
 		
-		CoordinateWorld3D coor3D = pars3D(lat, lon, fields[3]);	
+		while(line.isBlank() == false) {
+		
+			String[] fields = line.split(", ");
+			String id = fields[0];
+			String[] lat = fields[1].split(",");
+			String[] lon = fields[2].split(",");
+			
+			CoordinateWorld3D coor3D = pars3D(lat, lon, fields[3]);	
+			ComponentNavaidFix fix = new ComponentNavaidFix(id, coor3D);
+			
+			//navaids.put(id, fix); //this is causing a null pointer error
+			overlay.addNavaid(fix);
+			line = scanner.nextLine();
+		}
+	
 	}
 
 }
