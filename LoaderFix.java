bce7adfd5e923a62a59e0a1b6bc250aff6e8769e
Mamdouh Zayed <46635037+EWU123@users.noreply.github.com>
2021-11-22 13:39:27 -0800

    Create LoaderFix.java

---
+++ LoaderFix.java
@@ -0,0 +1,25 @@
+package atcsim.loader.navaid;
+
+import java.util.HashMap;
+
+import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.world.navigation.A_ComponentNavaid;
+
+public class LoaderFix {
+	public LoaderFix(A_ComponentNavaid navaids, OverlayNavigation overlay) {
+		
+	}
+
+	public LoaderFix(HashMap hm, OverlayNavigation on) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void load(java.util.Scanner scanner) throws java.io.IOException {
+		
+		
+	}
+		Readable Position() {
+			return null;
+		}
+	
+}
