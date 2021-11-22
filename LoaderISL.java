5d1839cdaec8ab5eb90e80ef9bc9b1c3597d4135
Mamdouh Zayed <46635037+EWU123@users.noreply.github.com>
2021-11-22 13:40:14 -0800

    Create LoaderISL.java

---
+++ LoaderISL.java
@@ -0,0 +1,16 @@
+package atcsim.loader.navaid;
+
+import java.util.HashMap;
+
+import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.world.navigation.A_ComponentNavaid;
+
+public class LoaderILS {
+	public LoaderILS(HashMap hm, OverlayNavigation overlay) {
+		
+	}
+	
+	public void load(java.util.Scanner scanner) throws java.io.IOException {
+		
+	}
+}
