b74f89cabebc3e9eb4ae293bfb0d610241b9f084
Justin Plett <jmplett@me.com>
2021-11-29 11:56:47 -0800

    JUnit test file

---
+++ src/DatatypeTester.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DatatypeTester {
+
+	@Test
+	void testAltitude() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testAngleNavigational() {
+		fail("Not yet implemented");
+	}
+	
+
+}
