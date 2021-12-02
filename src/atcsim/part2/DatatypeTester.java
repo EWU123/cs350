9dec03f5dbe88c05098f56c2cc857ecd504c9395
Zack Ward <79941828+ZackWard-student@users.noreply.github.com>
2021-12-01 16:43:23 -0800

    Implemented JUnit testing for CoordinateWorld and CoordinateWorld3D
    JUnit testing for CoordinateWorld that
    p1 == p1 s correct
    p1 + p2 is correct
    p2 + p1 is correct
    
    JUnit testing for CoordinateWorld3D that
    p == p angle and radius is correct
    p to KSFF_N angle and radius is correct
    p to KSFF_S angle and radius is correct
    p to KSFF_E angle and radius is correct
    p to KSFF_W angle and radius is correct

--- src/atcsim/part2/DatatypeTester.java
+++ src/atcsim/part2/DatatypeTester.java
@@ -50,28 +50,108 @@ class DatatypeTester {
 	void testCourse() {
 		Course c = new Course(10);
 		c = c.add_(new Course(0));
-		assertEquals(10,c.getValue_());
+		assertEquals(10, c.getValue_());
 		
 		c = c.add_(new Course(355));
-		assertEquals(5,c.getValue_());
+		assertEquals(5, c.getValue_());
 		
 		c = c.subtract_(new Course(0));
-		assertEquals(5,c.getValue_());
+		assertEquals(5, c.getValue_());
 		
 		c = c.subtract_(new Course(355));
-		assertEquals(10,c.getValue_());
+		assertEquals(10, c.getValue_());
 	}
 	
 	// Course is the campus degrees while attitude yaw is the direction in degrees in relation to the current attitude.
 
 	@Test
 	void testCoordinateWorld() {
-		fail("Not yet implemented");
 		
+		CoordinateWorld p1 = CoordinateWorld.KSFF;
+		CoordinateWorld p2 = new CoordinateWorld(new Latitude(1, 2, 3), new Longitude(3 ,2 ,1 ));
+		
+		int c;
+		double t;
+		
+		//p1 == p1
+		c = p1.getLatitude().compareTo(p1.getLatitude());
+		assertEquals(0, c);
+		
+		c = p1.getLongitude().compareTo(p1.getLongitude());
+		assertEquals(0, c);
+		
+		//testing p1 + p2
+		CoordinateWorld test = p1.add_(p2);
+		
+		//testing Latitude add
+		Latitude testLa = new Latitude(50, 41, 35.0); //expected
+		
+		c = test.getLatitude().getDegrees();
+		assertEquals(testLa.getDegrees(), c);
+		
+		t = test.getLatitude().getMinutesAndSeconds();
+		assertEquals(testLa.getMinutesAndSeconds(), t, .001);
+		
+		//testing Longitude add
+		Longitude testLo = new Longitude(120, 27, 31.0); //expected
+		
+		c = test.getLongitude().getDegrees();
+		assertEquals(testLo.getDegrees(), c);
+		
+		t = test.getLongitude().getMinutesAndSeconds();
+		assertEquals(testLo.getMinutesAndSeconds(), t, .001);
+		
+		//testing p2 + p1
+		test = p2.add_(p1);
+		
+		//testing Latitude add
+		testLa = new Latitude(50, 41, 35.0); //expected
+		
+		c = test.getLatitude().getDegrees();
+		assertEquals(testLa.getDegrees(), c);
+		
+		t = test.getLatitude().getMinutesAndSeconds();
+		assertEquals(testLa.getMinutesAndSeconds(), t, .001);
+		
+		//testing Longitude add
+		testLo = new Longitude(120, 27, 31.0); //expected
+		
+		c = test.getLongitude().getDegrees();
+		assertEquals(testLo.getDegrees(), c);
+		
+		t = test.getLongitude().getMinutesAndSeconds();
+		assertEquals(testLo.getMinutesAndSeconds(), t, .001);
 	}
 	
 	@Test
 	void testCorrdinateWorld3D() {
-		fail("Not yet implemented");
+		Altitude zero = new Altitude(0);
+		CoordinateWorld3D p = new CoordinateWorld3D(CoordinateWorld.KSFF, zero);
+		CoordinateWorld3D test;
+		
+		//testing that p == p with angle and radius
+		assertEquals(90, p.calculateBearing3D(p).getAngle().getValue_());
+		assertEquals(0, p.calculateBearing3D(p).getRadiusNauticalMiles().getValue_());
+		
+		//testing that p to KSFF_N with angle and radius is correct
+		test = new CoordinateWorld3D(CoordinateWorld.KSFF_N, zero);
+		assertEquals(0, p.calculateBearing3D(test).getAngle().getValue_(), .0001);
+		assertEquals(794.0382, p.calculateBearing3D(test).getRadiusNauticalMiles().getValue_(), .0001);
+		
+		//testing that p to KSFF_S with angle and radius is correct
+		test = new CoordinateWorld3D(CoordinateWorld.KSFF_S, zero);
+		assertEquals(180, p.calculateBearing3D(test).getAngle().getValue_(), .0001);
+		assertEquals(794.0382, p.calculateBearing3D(test).getRadiusNauticalMiles().getValue_(), .0001);
+		
+		//testing that p to KSFF_E with angle and radius is correct
+		test = new CoordinateWorld3D(CoordinateWorld.KSFF_E, zero);
+		assertEquals(90, p.calculateBearing3D(test).getAngle().getValue_(), .0001);
+		assertEquals(794.0382, p.calculateBearing3D(test).getRadiusNauticalMiles().getValue_(), .0001);
+		
+		//testing that p to KSFF_W with angle and radius is correct
+		test = new CoordinateWorld3D(CoordinateWorld.KSFF_W, zero);
+		assertEquals(270, p.calculateBearing3D(test).getAngle().getValue_(), .0001);
+		assertEquals(794.0382, p.calculateBearing3D(test).getRadiusNauticalMiles().getValue_(), .0001);
+		
 	}
 }
